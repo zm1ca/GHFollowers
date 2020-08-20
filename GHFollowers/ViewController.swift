85ac722da63ba062261b1664ac550ca455900376
zm1ca <zmicier.fied@yandex.ru>
2020-08-20 10:16:09 +0300

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Źmicier Fiedčanka on 20.08.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
