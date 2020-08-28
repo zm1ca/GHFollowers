a53b6bccc72d067e70fda89cf13f32085042abf9
zm1ca <zmicier.fied@yandex.ru>
2020-08-28 12:53:30 +0300

    28. HeaderInfoVC

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Źmicier Fiedčanka on 28.08.20.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
